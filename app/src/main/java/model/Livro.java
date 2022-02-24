d71980e11c52fb47667af4e37e35eef29c539401
PedroMonteiro7 <pedromonteirogt@gmail.com>
2022-02-24 11:25:23 -0300

    Estruturando o Recycler View

---
+++ app/src/main/java/model/Livro.java
@@ -0,0 +1,20 @@
+package model;
+
+// Manipula os valores de título e descrição (transporta dados)
+public class Livro {
+
+    private String titulo, descricao;
+
+    public Livro(String titulo, String descricao) {
+        this.titulo = titulo;
+        this.descricao = descricao;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+}
